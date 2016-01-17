45781b07e0167a513d0187cff02546174e03e3c4
Mohamed Carrim <crrmoh003@myuct.ac.za>
2016-01-17 19:35:00 +0200

    Add funds to goals
    Added functionality for adding funds from accounts to goals. Each goal
    is able to be updated from it's card on the main page.
    
    Messed around with styling and stuff just to get a feel for things.

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -11,12 +11,29 @@ app.controller('MainCtrl', [
             'goals': [{
                     'goal': "bike",
                     'target': 200,
-                    'current': 50
+                    'current': 50,
+					addedAmt: 0,
+					adding: false,
+					notAdding: true
                 }, {
                     goal: "shirt",
                     target: 50,
-                    current: 0
+                    current: 0,
+					addedAmt: 0,
+					adding: false,
+					notAdding: true
                 }
-            ]
+            ],
         };
+		$scope.addToGoal = function(index){
+			$scope.account.goals[index].current += $scope.account.goals[index].addedAmt;
+			$scope.account.balance -= $scope.account.goals[index].addedAmt;
+			$scope.account.goals[index].adding=false;
+			$scope.account.goals[index].notAdding=true;
+		};
+		
+		$scope.showAddBox = function(index){
+			$scope.account.goals[index].adding = true;
+			$scope.account.goals[index].notAdding = false;
+		};
     }]);
